4f36ea4ea0d6a9ec1099899be929c9beb5cd7365
vidyarthimanini <140050683+vidyarthimanini@users.noreply.github.com>
2025-12-18 13:26:47 +0530

    Update pan_validator.py

--- validation/pan_validator.py
+++ validation/pan_validator.py
@@ -1,14 +1,14 @@
 import re
 
-PAN_REGEX = r"[A-Z]{3}[PCHFTABGJKLE][A-Z][0-9]{4}[A-Z]"
+PAN_REGEX = r"^[A-Z]{3}[PCHFTABGJKLE][A-Z][0-9]{4}[A-Z]$"
 
 def validate_pan(pan: str):
     if not pan:
         return False, "PAN is mandatory"
 
-    pan = pan.upper().strip()
+    pan = pan.strip().upper()
 
     if not re.fullmatch(PAN_REGEX, pan):
-        return False, "Invalid PAN format"
+        return False, "Invalid PAN format (e.g. AAACR5055K)"
 
     return True, "VALID"
